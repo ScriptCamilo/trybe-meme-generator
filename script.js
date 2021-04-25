e39e7666edab415df0e3872cc73cffe90a6227f4
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-04-25 00:05:25 -0300

    Second requirement done

--- script.js
+++ script.js
@@ -11,6 +11,28 @@ function getInput() {
   input.addEventListener('input', setText);
 }
 
+function setFile(event) {
+  // source: https://developer.mozilla.org/en-US/docs/Web/API/File/Using_files_from_web_applications
+  const file = event.target.files[0];
+  const image = document.querySelector('#meme-image');
+
+  image.file = file;
+
+  const reader = new FileReader();
+  reader.onload = ((aImg) => (e) => {
+    const asyncImg = aImg;
+    asyncImg.src = e.target.result;
+  })(image);
+  reader.readAsDataURL(file);
+}
+
+function getFiles() {
+  const fileInput = document.querySelector('#meme-insert');
+
+  fileInput.addEventListener('change', setFile);
+}
+
 window.onload = () => {
   getInput();
+  getFiles();
 };
